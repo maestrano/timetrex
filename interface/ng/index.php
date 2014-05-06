00b8ab55f718abce12bb4470617b3e9564650055
Arnaud Lachaume <arnaud.lachaume@welwire.com>
2014-05-06 20:38:23 +1000

    added angular setup

---
+++ interface/ng/index.php
@@ -0,0 +1,102 @@
+<?php
+/*********************************************************************************
+ * TimeTrex is a Payroll and Time Management program developed by
+ * TimeTrex Software Inc. Copyright (C) 2003 - 2013 TimeTrex Software Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY TIMETREX, TIMETREX DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact TimeTrex headquarters at Unit 22 - 2475 Dobbin Rd. Suite
+ * #292 Westbank, BC V4T 2E9, Canada or at email address info@timetrex.com.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Powered by TimeTrex" logo. If the display of the logo is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Powered by TimeTrex".
+ ********************************************************************************/
+/*
+ * $Revision: 4104 $
+ * $Id: index.php 4104 2011-01-04 19:04:05Z ipso $
+ * $Date: 2011-01-04 11:04:05 -0800 (Tue, 04 Jan 2011) $
+ */
+require_once('../../includes/global.inc.php'); //Mainly to force redirect to SSL URL if required...
+
+// Hook:Maestrano
+// Load Maestrano
+$authentication = new Authentication();
+$maestrano = MaestranoService::getInstance();
+// Require authentication straight away if intranet
+// mode enabled
+if ($maestrano->isSsoIntranetEnabled()) {
+  if (!isset($_SESSION)) session_start();
+  if (!$maestrano->getSsoSession()->isValid()) {
+    header("Location: " . $maestrano->getSsoInitUrl());
+    exit;
+  }
+}
+
+// Hook:Maestrano
+if ($maestrano->isSsoEnabled()) {
+  if (!isset($_SESSION)) session_start();
+  if ($authentication->Check()) {
+    // Check Maestrano session is still valid
+    if (!$maestrano->getSsoSession()->isValid()) {
+      header("Location: " . $maestrano->getSsoInitUrl());
+      exit;
+    }
+  } else {
+    // Redirect to login
+    header("Location: " . $maestrano->getSsoInitUrl());
+    exit;
+  }
+}
+
+Misc::redirectMobileBrowser(); //Redirect mobile browsers automatically.
+
+?>
+<html lang="en">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>TimeTrex - Secure Login</title>
+<?php if ($maestrano->isSsoEnabled()) { ?>
+<script>
+var logoutRedirect = "<?php echo $maestrano->getSsoLogoutUrl();?>";
+var ssoLoginRedirect = "<?php echo $maestrano->getSsoInitUrl();?>";
+setInterval(function(){
+  if (document.cookie.indexOf('timetrex_logout=1') != -1) {
+    document.cookie = 'timetrex_logout=; expires=Thu, 01-Jan-70 00:00:01 GMT; path=/';
+    window.location.replace(logoutRedirect);
+  }
+  if (document.cookie.indexOf('timetrex_relogin=1') != -1) {
+    document.cookie = 'timetrex_relogin=; expires=Thu, 01-Jan-70 00:00:01 GMT; path=/';
+    window.location.replace(ssoLoginRedirect);
+  }
+},1000);
+</script>
+<?php } ?>
+<script src="/interface/js/angular/angular.min.js" language="javascript"></script>
+</head>
+<body>
+</body>
+</html>
