4fb4dcb7e01f38b6b27c1fd1a8805fc77766c7a2
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-03-07 15:23:33 +1100

    Give table permissions to Postgres timetrex users

--- maestrano/app/db/2_add_mno_id_map.sql
+++ maestrano/app/db/2_add_mno_id_map.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS `mno_id_map` (
-  `mno_entity_guid` varchar(255) NOT NULL,
-  `mno_entity_name` varchar(255) NOT NULL,
-  `app_entity_id` varchar(255) NOT NULL,
-  `app_entity_name` varchar(255) NOT NULL,
-  `db_timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_flag` int(1) NOT NULL DEFAULT '0'
+CREATE TABLE IF NOT EXISTS mno_id_map (
+  mno_entity_guid varchar(255) NOT NULL,
+  mno_entity_name varchar(255) NOT NULL,
+  app_entity_id integer NOT NULL,
+  app_entity_name varchar(255) NOT NULL,
+  db_timestamp timestamp NOT NULL DEFAULT LOCALTIMESTAMP(0),
+  deleted_flag smallint NOT NULL DEFAULT 0
 );
+ALTER TABLE public.mno_id_map OWNER TO timetrex;
