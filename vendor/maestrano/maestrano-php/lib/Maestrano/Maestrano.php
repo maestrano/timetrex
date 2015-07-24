8969a1a44fb9cfedf602edca45084a46e45a2100
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-07-24 14:28:04 +1000

    Add scripts folder

--- vendor/maestrano/maestrano-php/lib/Maestrano/Maestrano.php
+++ vendor/maestrano/maestrano-php/lib/Maestrano/Maestrano.php
@@ -282,7 +282,7 @@ class Maestrano
         'api.base'               => '/api/v1/',
         'connec.enabled'         => true,
         'connec.host'            => 'http://connec.maestrano.io',
-        'connec.base_path'       => '/connec/api',
+        'connec.base_path'       => '/api',
         'connec.v2_path'         => '/v2',
         'connec.reports_path'    => '/reports',
         'sso.idp'                => 'http://application.maestrano.io',
